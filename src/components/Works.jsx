140c1e091cab7e83b40b73a005d80cea3502fc13
xavier <ulgenmf@gmail.com>
2023-03-31 18:58:20 +0200

    c

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -36,7 +36,7 @@ const ProjectCard = ({
 					<div className="absolute inset-0 flex justify-end m-3 card-img_hover">
 						<div
 							onClick={() => window.open(source_code_link, "_blank")}
-							className="black-gradient  rounded-full flex justify-center items-center cursor-pointer"
+							className="black-gradient w-10 h-10  rounded-full flex justify-center items-center cursor-pointer"
 						>
 							<img
 								loading="lazy"
